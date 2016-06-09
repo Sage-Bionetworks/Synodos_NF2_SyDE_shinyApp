bedf790057fdd19262ed95633a6343256dad7d84
xindiguo <xindi.guo@sagebase.org>
2016-06-09 13:54:16 -0700

    change dataset

--- drugScreenModule.R
+++ drugScreenModule.R
@@ -4,6 +4,10 @@ drugScreenModuleUI <- function(id){
     myHeader <- dashboardHeader(title="Drug Screen Demo", disable=TRUE),
     mySidebar <- dashboardSidebar(disable=TRUE),
     myBody <- dashboardBody(
+      tags$head(tags$style(HTML('
+                            .btn {
+                            float:right;
+                            }'))),
       fluidRow(
         box(width=12, status='primary', collapsible=TRUE, 
             collapsed=FALSE, solidHeader=TRUE,
@@ -31,28 +35,23 @@ drugScreenModuleUI <- function(id){
             #uiOutput(ns("filters"))
             column(width=3,
                    # Max Response filter 
-                   sliderInput(ns('maxResp'), 'Max Response', 
+                   sliderInput(ns('maxR_filter'), 'Max Response', 
                                min=1, max=100, value=c(1, 100), 
-                               step=10, round=0)
+                               step=10, round=TRUE)
             ),
             column(width=3,
-               sliderInput(ns('ic50_filter'), 'IC50',
-                    min=10, max=100, value=c(1, 1000), 
-                    step=5, round=0)
+                   # IC50 filter
+                   sliderInput(ns('ic50_filter'), 'IC50 (uM)',
+                               min=10, max=100, value=c(1, 10), 
+                               step=5)
             ),
             column(width=3,
-              # AC50 filter
-              uiOutput(ns("filter1"))
-#               sliderInput(ns('ac50_filter'), 'AC50', 
-#                          min=10, max=100, value=c(1, 1000), 
-#                          step=5, round=0)
+                   # AC50 filter
+                   uiOutput(ns("filter1"))
             ),
             column(width=3,
-              # Curve class filter
-              uiOutput(ns("filter2"))
-#               sliderInput(ns('curveClass'), 'Curve Class', 
-#                           min=-10, max=10, value=sort(unique(drugScreenData$curveClass)), 
-#                           step=1, round=0)
+                   # Curve class filter
+                   uiOutput(ns("filter2"))
             )
         )
        ),
@@ -60,24 +59,30 @@ drugScreenModuleUI <- function(id){
       fluidRow(
              tabBox(width = 12,
                tabPanel("Max Response",
-                 plotOutput(ns("drug_max_resp"),height="700px",width="auto",hoverId=NULL)
+                 plotOutput(ns("drug_max_resp"),height="500px",width="auto",hoverId=NULL)
               ),
               tabPanel("IC50",
-                 plotOutput(ns("drugScreen_IC50_plot"),height="700px",width="auto",hoverId=NULL)
+                 plotOutput(ns("drugScreen_IC50_plot"),height="500px",width="auto",hoverId=NULL)
               ),
               tabPanel("AC50",
-                 plotOutput(ns("drugScreen_AC50_plot"),height="700px",width="auto",hoverId=NULL)
-              ),
-              tabPanel("QC",
-                h5("Density Histograms Plots"),
-                plotOutput(ns("QC_plots"),height="700px")
+                 plotOutput(ns("drugScreen_AC50_plot"),height="500px",width="auto",hoverId=NULL)
               ),
               tabPanel("Dose Response",
+                       helpText("If more than 8 drugs are selected, only the first 8 drugs will be showing."),
+                #checkboxInput(ns("replicate"), "use \"replicate\""),
+                #br(),
                 plotOutput(ns("doseResp_plot"))
               ),
               tabPanel("Data",
                 downloadButton(ns("downloadData")),
+                br(),
+                br(),
+                br(),
                 dataTableOutput(ns("drugScreen_dataTable"))
+              ),
+              tabPanel("QC",
+                       h5("Density Histograms Plots"),
+                       plotOutput(ns("QC_plots"),height="500px")
               )
              )
       )
@@ -114,7 +119,7 @@ drugScreenModule <- function(input,output,session,summarizedData = NULL, rawData
     }
   })
   
-  output$filter1 <- renderUI({
+  output$filter2 <- renderUI({
     if(!all(is.na(summarizedData$curveClass))){
       tagList(
         sliderInput(ns('curveClass'), 'Curve Class', 
@@ -124,11 +129,8 @@ drugScreenModule <- function(input,output,session,summarizedData = NULL, rawData
     }
   })
  
-  
   get_selected_samples <- reactive({
     samples <- input$samples
-    validate(need(length(samples) != 0, "At least one sample needs to be selected" ))
-    validate(need(length(samples) <= 5, "You can select up to 5 samples" ))
     samples
   })
   
@@ -142,6 +144,9 @@ drugScreenModule <- function(input,output,session,summarizedData = NULL, rawData
   })
   
   get_drug_data <- eventReactive(input$updateButton,{
+      validate(need(!is.null(input$samples), "At least one sample needs to be selected" ))
+      validate(need(length(input$samples) <= 5, "You can select up to 5 samples" ))
+      validate(need(!is.null(input$selected_drugs), "At least one sample needs to be selected" ))
       flt_drug_data <- filter(summarizedData, drug %in% get_selected_drugs())  
       flt_drug_data <- filter(flt_drug_data, sample %in% get_selected_samples())  
       return(flt_drug_data)
@@ -152,41 +157,46 @@ drugScreenModule <- function(input,output,session,summarizedData = NULL, rawData
     drug_data <- get_drug_data()
     
     # max response
-    mR_min <- floor(min(drug_data$maxResp,na.rm = T))
-    mR_max <- ceiling(max(drug_data$maxResp,na.rm = T))
-    updateSliderInput(session, "maxResp", min = mR_min, max = mR_max, value = c(mR_min,mR_max))
-    
+   # if(!all(is.na(summarizedData$maxResp))){
+      mR_min <- floor(min(drug_data$maxResp,na.rm = T))
+      mR_max <- ceiling(max(drug_data$maxResp,na.rm = T))
+      updateSliderInput(session, "maxR_filter", min = mR_min, max = mR_max, value = c(mR_min,mR_max))
+  #  }
     # AC50 slider
-    if(!all(is.na(summarizedData$AC50))){
-      ac50_min <- floor(min(drug_data$AC50,na.rm = T))
-      ac50_max <- ceiling(max(drug_data$AC50,na.rm = T))
-      updateSliderInput(session, "ac50_filter", max = ac50_max, value = c(ac50_min,ac50_max))
-    }
+#     if(!all(is.na(summarizedData$AC50))){
+#       ac50_min <- floor(min(drug_data$AC50,na.rm = T))
+#       ac50_max <- ceiling(max(drug_data$AC50,na.rm = T))
+#       updateSliderInput(session, "ac50_filter", max = ac50_max, value = c(ac50_min,ac50_max))
+#     }
     # IC50 slider
-     ic50_min <- floor(min(drug_data$IC50,na.rm = T))
-     ic50_max <- ceiling(max(drug_data$IC50,na.rm = T))
-     updateSliderInput(session, "ic50_filter", min = ic50_min, max = ic50_max, value = c(ic50_min, ic50_max))
-    
-    # curve class
-     if(!all(is.na(summarizedData$curveClass))){
-      cc_min <- floor(min(drug_data$curveClass,na.rm = T))
-      cc_max <- ceiling(max(drug_data$curveClass,na.rm = T))
-      updateSliderInput(session, "curveClass", min = cc_min, max = cc_max, value = c(cc_min,cc_max))
-     }
+    #if(!all(is.na(summarizedData$IC50))){
+      ic50 <- drug_data$IC50
+      ic50 <- ic50[!is.na(ic50)]
+      ic50 <- ic50[!is.infinite(ic50)]
+      ic50_min <- floor(min(ic50))
+      ic50_max <- ceiling(max(ic50))
+      updateSliderInput(session, "ic50_filter", min = ic50_min, max = ic50_max, value = c(ic50_min, ic50_max))
+    #}
+#     # curve class
+#      if(!all(is.na(summarizedData$curveClass))){
+#       cc_min <- floor(min(drug_data$curveClass,na.rm = T))
+#       cc_max <- ceiling(max(drug_data$curveClass,na.rm = T))
+#       updateSliderInput(session, "curveClass", min = cc_min, max = cc_max, value = c(cc_min,cc_max))
+#      }
   })
 
   get_filtered_drug_data <- reactive({
     drug_data <- get_drug_data()
-    filtered_data <- drug_data[drug_data$maxResp >= input$maxResp[1] & drug_data$maxResp <= input$maxResp[2],]
-    if(!all(is.na(summarizedData$curveClass))){
-      filtered_data <- drug_data[drug_data$curveClass >= input$curveClass[1] & drug_data$curveClass <= input$curveClass[2],]
-    }
-    if(!all(is.na(summarizedData$AC50))){
-      filtered_data <- drug_data[drug_data$AC50 >= input$ac50_filter[1] & drug_data$AC50 <= input$ac50_filter[2],]
-    }
-    if(!all(is.na(summarizedData$IC50))){
+    filtered_data <- drug_data[drug_data$maxResp >= input$maxR_filter[1] & drug_data$maxResp <= input$maxR_filter[2],]
+    #if(!all(is.na(summarizedData$curveClass))){
+    #  filtered_data <- drug_data[drug_data$curveClass >= input$curveClass[1] & drug_data$curveClass <= input$curveClass[2],]
+    #}
+    #if(!all(is.na(summarizedData$AC50))){
+    #  filtered_data <- drug_data[drug_data$AC50 >= input$ac50_filter[1] & drug_data$AC50 <= input$ac50_filter[2],]
+   # }
+   # if(!all(is.na(summarizedData$IC50))){
       filtered_data <- drug_data[drug_data$IC50 >= input$ic50_filter[1] & drug_data$IC50 <= input$ic50_filter[2],]
-    }
+  #  }
     filtered_data <- filtered_data[!is.na(filtered_data$sample),]
     filtered_data
   })
@@ -204,7 +214,6 @@ drugScreenModule <- function(input,output,session,summarizedData = NULL, rawData
   
   # Max Response Plot
   output$drug_max_resp <- renderPlot({
-    validate(need(input$updateButton, "Please select samples and drugs, then click \"Update\" button." ))
     flt_drug_data <- get_filtered_drug_data()
     drug_levels <- flt_drug_data %>%
       group_by(drug) %>%
@@ -220,22 +229,22 @@ drugScreenModule <- function(input,output,session,summarizedData = NULL, rawData
   
   # IC50 plot
   output$drugScreen_IC50_plot <- renderPlot({
-    validate(need("IC50" %in% plot_names, "IC50 data does not exist." ))
     flt_drug_data <- get_filtered_drug_data()
     #remove NA and Inf
     flt_drug_data <- flt_drug_data[! is.na(flt_drug_data$IC50), ]
     flt_drug_data <- flt_drug_data[! is.infinite(flt_drug_data$IC50), ]
     #convert to log10
-    flt_drug_data["IC50"] <- log10(as.numeric(flt_drug_data[,"IC50"])*1e+6) 
+    flt_drug_data$IC50 <- log10(as.numeric(flt_drug_data$IC50)) 
     drug_levels <- flt_drug_data %>%
       group_by(drug) %>%
       summarise(med=median("IC50", na.rm=T)) %>%
       arrange(desc(med)) %>% select(drug)
     drug_levels <- drug_levels$drug
     flt_drug_data$drug <- factor(flt_drug_data$drug,levels=drug_levels)
+    flt_drug_data <- flt_drug_data[order(flt_drug_data$IC50,decreasing = T),] 
     p <- ggplot(data=flt_drug_data, aes_string(x="drug", y="IC50", group="sample")) 
     p <- p + geom_point(aes(color=sample), size=3) + theme_bw(base_size = 15)
-    p + theme(axis.text.x=element_text(angle=x_angle()[1], hjust=x_angle()[2])) + xlab('Drug') + ylab('IC50')
+    p + theme(axis.text.x=element_text(angle=x_angle()[1], hjust=x_angle()[2])) + xlab('Drug') + ylab('log 10 IC50')
   })
   
   # AC50 plot
@@ -279,15 +288,25 @@ drugScreenModule <- function(input,output,session,summarizedData = NULL, rawData
   output$doseResp_plot <- renderPlot({
     drug_data <- get_filtered_drug_data()
     drugs <- unique(drug_data$drug)
+    if(length(drugs) > 8){
+      drugs <- drugs[1:8]
+    }
     samples <- unique(drug_data$sample)
     normData <- rawData[rawData$sample %in% samples & rawData$drug %in% drugs,]
-    doseRespData <- ddply(.data=normData, .variables = c('drug', 'sample'),.fun = tmp_iterator, .parallel = T)
-    #missing normData and doseRespData, should be filtered and reactive
+    var <- c('drug', 'sample')
+#     if(input$replicate){
+#       var <- c(var,"replicate")
+#     }
+    doseRespData <- ddply(.data=normData, .variables = var,.fun = tmp_iterator, .parallel = T)
+    doseRespData$grp <- doseRespData$sample
+#     if(input$replicate){
+#       doseRespData$grp <- paste(doseRespData$sample,doseRespData$replicate)
+#     }
     p <- ggplot(normData, aes(x = log10(conc*(1e+6)), y = normViability*100)) 
     p <- p + geom_point(aes_string(color="sample")) 
     p <- p + scale_color_brewer(type = "qual", palette = 2, direction = 1)
-    p <- p + geom_line(data = doseRespData, aes(x = fittedX+6, y = fittedY*100, colour = sample, group = sample))
-    p <- p + facet_grid(. ~ drug) + theme_bw(base_size = 15)
+    p <- p + geom_line(data = doseRespData, aes(x = fittedX+6, y = fittedY*100, colour = sample, group = grp))
+    p <- p + facet_wrap(~ drug, ncol = 4) + theme_bw(base_size = 15)
     p <- p + geom_hline(aes(yintercept=50), color='grey50', linetype='dashed')
     p <- p + xlab('log10 micromolar conc') + ylab('cell viability %') 
     p
@@ -299,7 +318,7 @@ drugScreenModule <- function(input,output,session,summarizedData = NULL, rawData
   )
   
   output$downloadData <- downloadHandler(
-    filename = function() { 'demo.csv' },
+    filename = function() { 'summarizedData.csv' },
     content = function(file) {
       write.csv(get_filtered_drug_data(), file)
     }
--- getData.R
+++ getData.R
@@ -1,56 +1,29 @@
 ############
 # Raw Data
 ############
-#download the DMSO norm data for MGH and UCF Drug Screens
-UCF_normViab <- 'syn2773870'
-UCF_normViab <- synGet(UCF_normViab)
-UCF_normViab <- read.delim(UCF_normViab@filePath, check.names=F, sep="\t", header=T)
-UCF_normViab$cellLine <- gsub("^ ", "", UCF_normViab$cellLine)
-UCF_normViab$cellLine <- gsub("Nf2 --", "Nf2--", UCF_normViab$cellLine)
 
-MGH_normViab <- 'syn2773792'
-MGH_normViab <- synGet(MGH_normViab)
-MGH_normViab <- read.delim(MGH_normViab@filePath, check.names=F, sep="\t", header=T)
-MGH_normViab$cellLine <- gsub("^ ", "", MGH_normViab$cellLine)
-
-#drop unnecassary cols
-drop_cols <- c('plate', 'medianDMSO', 'viability', 'experiment','stage')
-MGH_normViab <- MGH_normViab[, !colnames(MGH_normViab) %in% drop_cols]
-UCF_normViab <- UCF_normViab[, !colnames(UCF_normViab) %in% drop_cols]
-
-#align the columns of the two dataframe before combining them
-UCF_normViab <- UCF_normViab[,colnames(MGH_normViab)]
-
-#combined drug normViab
-#rbindlist is fastest for concatenating data.frames/ data.tables by row
-rawData <- as.data.frame(rbindlist(list(UCF_normViab, MGH_normViab))) 
+rawData <- "syn6138251"
+rawData <- synGet(rawData)
+rawData <- read.delim(rawData@filePath, check.names=F, sep="\t", header=T)
 colnames(rawData)[1] <- "sample"
-
+select_col <- c("sample", "conc", "drug", "replicate", "normViability")
+rawData <- rawData[,select_col]
 
 #################
 # Summarized Data
 #################
 
-#download the precomputed IC Vals for MGH and UCF Data
-UCF_ICvals <- 'syn2773891'
-UCF_ICvals <- synGet(UCF_ICvals)
-UCF_ICvals <- read.delim(UCF_ICvals@filePath, check.names=F, sep="\t", header=T)
+summarizedData <- "syn6138237"
+summarizedData <- synGet(summarizedData)
+summarizedData <- read.delim(summarizedData@filePath, check.names=F, sep="\t", header=T)
 
-MGH_ICvals <- 'syn2773794'
-MGH_ICvals <- synGet(MGH_ICvals)
-MGH_ICvals <- read.delim(MGH_ICvals@filePath, check.names=F, sep="\t", header=T)
-
-#align the columns of the two dataframe before combining them
-MGH_ICvals <- UCF_ICvals[,colnames(UCF_ICvals)]
-
-#combined 
-summarizedData <- as.data.frame(rbindlist(list(MGH_ICvals, UCF_ICvals)))
-select_col <- c("cellLine","drug","IC50","maxEfficacy")
+select_col <- c("cellLine","drug","IC50","maxEfficacy","trapezoid")
 summarizedData <- summarizedData[,select_col]
-colnames(summarizedData) <- c("sample", "drug", "IC50", "maxResp")
+colnames(summarizedData) <- c("sample", "drug", "IC50", "maxResp","AUC")
+
 summarizedData$maxResp <- summarizedData$maxResp*100
+summarizedData$IC50 <- summarizedData$IC50*1e+6
 summarizedData$AC50 <- NA
 summarizedData$target <- NA
-summarizedData$AUC <- NA
 summarizedData$curveClass <- NA
 
