ac54695423393f23a36c0d22a3681380ff9bb3f1
xindiguo <xindi.guo@sagebase.org>
2016-06-13 14:31:01 -0700

    edit module function

--- ui.R
+++ ui.R
@@ -2,7 +2,7 @@ shinyUI(navbarPage("Synodos NF2 Data Explorer",
 	#google analytics
 	header=list(tags$head(includeScript("www/google_analytics.js"))),
    tabPanel("Drug Screens",
-            drugScreenModuleUI("demo") 
+            drugScreenModuleUI(id = "demo", data = summarizedData) 
     ) # END tabPanel
   )#END navnarPage
 )#END shinyUI
