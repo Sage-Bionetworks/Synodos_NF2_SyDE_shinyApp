f3e407467797545af146dcd876ec07b7109ef713
Robert Allaway <allaway@users.noreply.github.com>
2019-01-29 11:27:30 -0800

    uncomment

--- server.R
+++ server.R
@@ -4,11 +4,11 @@ shinyServer(function(input, output, session) {
     
     synLogin(sessionToken=input$cookie)
     message(sprintf("Welcome, %s", synGetUserProfile()$userName))
-    #source("getData.R")
+    source("getData.R")
 
-    #callModule(drugScreenModule,id = "demo",session = session, summarizedData = summarizedData, rawData = rawData,tag="demo")
-    #callModule(expressionViewerModule,id = "RNA",session = session,RNAseq,pathways_list,tag="RNA")
-    #callModule(kinomeViewerModule,id = "kinome",session = session,Syn5.Syn1.base=Syn5.Syn1.base,HS01.HS11.base=HS01.HS11.base,kinometx,tag="kinome")
+    callModule(drugScreenModule,id = "demo",session = session, summarizedData = summarizedData, rawData = rawData,tag="demo")
+    callModule(expressionViewerModule,id = "RNA",session = session,RNAseq,pathways_list,tag="RNA")
+    callModule(kinomeViewerModule,id = "kinome",session = session,Syn5.Syn1.base=Syn5.Syn1.base,HS01.HS11.base=HS01.HS11.base,kinometx,tag="kinome")
   })
 })
   
